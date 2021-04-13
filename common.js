d537581221a666ddf1a3d09a7f08465a11477972
xueke.song <15235592139@126.com>
2021-04-13 18:09:31 +0800

    node intro

---
+++ common.js
@@ -0,0 +1,8 @@
+/**
+ * 公共属性/方法
+ */
+
+// 数字和
+exports.add = function (a, b) {
+  return a + b;
+}
---
+++ hello.js
@@ -0,0 +1,8 @@
+/**
+ * 引入外部模块
+ */
+const common = require('./common')
+
+console.log('hello Node ' + common.add(1,2))
+
+// console.log(arguments.length)
